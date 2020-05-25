df4474091ebc76c9a011615deb2af23158815d31
vic-algonquin <pham0149@algonquinlive.com>
2020-05-25 00:08:05 -0400

    added

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+var body = document.querySelector("#gradient");
+var button = document.querySelector("button");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-	css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ";";
+}
+
+function setRandomGradient() {
+  body.style.background =
+    "linear-gradient(to right, " +
+    generateRandomColors() +
+    ", " +
+    generateRandomColors();
+
+  css.textContent = body.style.background + ";";
+}
+
+function generateRandomColors() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
+button.addEventListener("click", setRandomGradient);
